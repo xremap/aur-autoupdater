07e27aa7763e0525212c0d9ab4cfe0e41587c6b4
Nilesh <njkevlani@gmail.com>
2023-02-20 23:05:33 +0530

    Better error handling for GitHub
    GH Actions were intermittently failing. Example for such failure:
    https://github.com/njkevlani/aur-autoupdater/actions/runs/4219050390
    
    This looks like a result from non-2xx response from GitHub, hence checking result
    before using it.

--- go.mod
+++ go.mod
@@ -5,8 +5,10 @@ go 1.19
 require (
 	github.com/go-git/go-billy/v5 v5.3.1
 	github.com/go-git/go-git/v5 v5.4.2
+	github.com/go-resty/resty/v2 v2.7.0
 	github.com/mikkeloscar/gopkgbuild v0.0.0-20211012125930-1f52fd970155
 	github.com/sirupsen/logrus v1.9.0
+	golang.org/x/crypto v0.0.0-20220817201139-bc19a97f63c8
 )
 
 require (
@@ -21,7 +23,6 @@ require (
 	github.com/mitchellh/go-homedir v1.1.0 // indirect
 	github.com/sergi/go-diff v1.2.0 // indirect
 	github.com/xanzy/ssh-agent v0.3.0 // indirect
-	golang.org/x/crypto v0.0.0-20220817201139-bc19a97f63c8 // indirect
 	golang.org/x/net v0.0.0-20211112202133-69e39bad7dc2 // indirect
 	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
 	gopkg.in/warnings.v0 v0.1.2 // indirect
--- internal/latestversion/latestversion.go
+++ internal/latestversion/latestversion.go
@@ -1,9 +1,9 @@
 package latestversion
 
 import (
-	"encoding/json"
 	"errors"
 	"fmt"
+	"github.com/go-resty/resty/v2"
 	"net/http"
 
 	"github.com/njkevlani/aur-autoupdater/internal/version"
@@ -14,28 +14,38 @@ import (
 func GetLatestVersion(owner, repo string) (version.Version, error) {
 	url := fmt.Sprintf("https://api.github.com/repos/%s/%s/releases/latest", owner, repo)
 
-	resp, err := http.Get(url)
+	versionFromGitHub := LatestGitHubVersion{}
+	resp, err := resty.New().R().
+		SetResult(&versionFromGitHub).
+		Get(url)
+
 	if err != nil {
-		logrus.WithError(err).WithField("url", url).Error("failed to get latest release info")
+		logrus.WithError(err).
+			WithField("resp", resp).
+			WithField("url", url).
+			Error("got error while making request to GitHub")
+
 		return nil, err
 	}
 
-	defer resp.Body.Close()
+	if resp.StatusCode() != http.StatusOK {
+		err = errors.New("got non-200 response from GitHub")
+		logrus.WithError(err).
+			WithField("resp", resp).
+			WithField("url", url).
+			Error()
 
-	version := LatestGitHubVersion{}
-	err = json.NewDecoder(resp.Body).Decode(&version)
-
-	if err != nil {
-		logrus.WithError(err).WithField("url", url).Error("failed to decode response")
 		return nil, err
 	}
 
-	if len(version.Version()) == 0 {
+	// TODO: This should not be required after better error handling above.
+	// If that is the cases, this will be removed in upcoming commits.
+	if len(versionFromGitHub.Version()) == 0 {
 		logrus.Error("latest version length is 0")
 		return nil, errors.New("latest version length is 0")
 	}
 
-	logrus.WithField("LatestGitHubVersion", fmt.Sprintf("%#v", version)).Info("LatestGitHubVersion")
+	logrus.WithField("LatestGitHubVersion", fmt.Sprintf("%#v", versionFromGitHub)).Info("LatestGitHubVersion")
 
-	return &version, nil
+	return &versionFromGitHub, nil
 }
